32a426ff2b825d5dca48345b25cea4c6432a462f
Alexandros Efthymiadis <alexandros.efthymiadis@upstreamsystems.com>
2021-08-25 20:21:38 +0300

    support a more dynamic way for finding the battery information for any Razer device supported by OpenRazer

--- install/_scripts/razer-battery-level.py
+++ install/_scripts/razer-battery-level.py
@@ -1,26 +1,71 @@
 #!/bin/env python
+
+'''
+This script is used to output the battery level of any Razer Device supported by the OpenRazer daemon.
+'''
+
+import argparse
+
 from openrazer.client import DeviceManager
 
-IS_CHARGING='🔌'
-ON_BATTERY='🔋'
-NOT_CONNECTED='❌'
+STATUS_DICT = {
+    'Charging . . .': "\U0001F50C",
+    'On battery': "\U0001F50B",
+    'Disconnected': "\U0000274C"
+}
+
+DEVICE_TYPE = {
+    'keyboard' : "\U0001F5AE", 
+    'mouse' : "\U0001F5B1", 
+    'headset' : "\U0001F3A7", 
+}
 
 # Create a DeviceManager. This is used to get specific devices
 device_manager = DeviceManager()
 
-# Iterate over each device and print its battery level
+# Helper function to recognize boolean arguments
+def str_to_bool(value):
+    if isinstance(value, bool):
+        return value
+    if value.lower() in ('true', 't'):
+        return True
+    elif value.lower() in ('false', 'f'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected')
+
+# Parser for command-line options
+args_parser = argparse.ArgumentParser('Script for retrieving battery information of Razer devices')
+required_arguments = args_parser.add_argument_group(title='Required Arguments')
+required_arguments.add_argument("--info", "-i", nargs='?', default=False, type=str_to_bool, help="print device info", required=False)
+required_arguments.add_argument("--type", "-t", help="device type", choices=['keyboard', 'mouse', 'mousemat', 'core', 'keypad', 'headset', 'accessory'], required=False)
+
+parameters = vars(args_parser.parse_args())
+is_info_enabled = parameters['info']
+device_type = parameters['type']
+
+# Iterate over each device
 for device in device_manager.devices:
+    
+    if device_type is not None:
+        if device_type != device.type:
+            continue
 
     status = ''
     battery_level = device.battery_level;
     if device.is_charging:
-        status = IS_CHARGING
+        status = 'Charging . . .'
     else:
         if battery_level > 0:
-            status = ON_BATTERY
+            status = 'On battery'
         else:
-            status = NOT_CONNECTED
-            print ("{}: {} not connected".format(device.name, status))
-            exit()
-
-    print ("{}: {} {}%".format(device.name, status, battery_level))
+            status = 'Disconnected'
+            
+    if is_info_enabled:
+        print ("{}\nStatus: {} {}".format(device.name, STATUS_DICT[status], status))
+        exit()
+        
+    if device_type is not None and (device_type == 'keyboard' or device_type == 'mouse' or device_type == 'headset'):
+        print ("{} {}%".format(DEVICE_TYPE[device_type], battery_level))
+    else:
+        print ("{} {}%".format(STATUS_DICT[status], battery_level))
